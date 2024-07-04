258e6ec2abcef49c516d5231e5ec660c7e955afc
CHONAN Tsuyoshi <s1310125@sshsv212.u-aizu.ac.jp>
2024-07-04 17:34:58 +0900

    Final revision

--- uraomote.c
+++ uraomote.c
@@ -9,14 +9,13 @@ const char* coin_toss() {
 int main() {
     int heads_count = 0;
     int tails_count = 0;
+    char user_name[100];
 
-    // 乱数のシードを設定
     srand(time(NULL));
 
     printf("Who are you?\n> ");
     scanf("%99s", user_name);
     printf("Hello, %s!\n", user_name);
-
 
     printf("Tossing a coin...\n");
     for (int round = 1; round <= 3; ++round) {
@@ -31,10 +30,10 @@ int main() {
 
     printf("Heads: %d, Tails: %d\n", heads_count, tails_count);
 
-     if (heads_count > tails_count) {
-        printf("%s won!\n",user_name);
+    if (heads_count > tails_count) {
+        printf("%s won!\n", user_name);
     } else {
-        printf("%s lost!\n",user_name);
+        printf("%s lost!\n", user_name);
     }
 
     return 0;
